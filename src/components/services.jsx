63bbdc598e1123d690407ca4112f5df0b49ac69f
venkatesh02040 <ysvh.pandu@gmail.com>
2025-02-21 21:58:46 +0530

    m

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,22 +7,22 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            PrepXpert provides industry-aligned placement tests, detailed performance analysis,
+            and skill enhancement resources to help candidates prepare effectively for their dream jobs.
           </p>
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <div className="service-desc">
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
-                  </div>
+              <div key={`${d.name}-${i}`} className="col-md-4">
+                {" "}
+                <i className={d.icon}></i>
+                <div className="service-desc">
+                  <h3>{d.name}</h3>
+                  <p>{d.text}</p>
                 </div>
-              ))
+              </div>
+            ))
             : "loading"}
         </div>
       </div>

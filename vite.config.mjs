dbf665843d5a485cba46b39170385094622941ec
venkatesh02040 <ysvh.pandu@gmail.com>
2025-02-26 15:08:59 +0530

    migration

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { terser } from '@rollup/plugin-terser';
+import terser from "@rollup/plugin-terser";
 
 export default defineConfig({
   plugins: [react(), terser()],

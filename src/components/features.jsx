a7a1ae3089344f3aed74178388b8606355d07092
venkatesh02040 <ysvh.pandu@gmail.com>
2025-02-26 09:50:26 +0530

    m

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -4,21 +4,28 @@ export const Features = (props) => {
   return (
     <div id="features" className="text-center">
       <div className="container">
-        <div className="col-md-10 col-md-offset-1 section-title" style={{marginTop:"100px"}}>
+        <div className="col-md-10 col-md-offset-1 section-title" style={{ marginTop: "100px" }}>
           <h2>Why Prepxpert</h2>
         </div>
-        <div className="row"id="fd">
+        <div className="row" id="fd">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3" id="lc" style={{cursor:"pointer"}}>
-                  {" "}
-                  <i className={d.icon}></i>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
-                </div>
-              ))
+              <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3" id="lc" style={{ cursor: "pointer" }}>
+                {" "}
+                <i className={d.icon}></i>
+                <h3>{d.title}</h3>
+                <p>{d.text}</p>
+              </div>
+            ))
             : "Loading..."}
         </div>
+        <br />
+        <a
+          href="#features"
+          className="btn btn-custom btn-lg page-scroll"
+        >
+          Start Your Journey
+        </a>{" "}
       </div>
     </div>
   );
